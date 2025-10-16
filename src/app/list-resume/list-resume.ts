c611de7bf9a089e10e8e41328795c7d3983b462f
Peter-McKinney <45462076+Peter-McKinney@users.noreply.github.com>
2025-10-16 13:37:03 -0500

    Add copy url and button styling

--- src/app/list-resume/list-resume.ts
+++ src/app/list-resume/list-resume.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { ResumeStore } from '../services/resume.store';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterLink } from '@angular/router';
-import { DatePipe } from '@angular/common';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-list-resume',
@@ -13,4 +13,10 @@ import { DatePipe } from '@angular/common';
 export class ListResume {
   readonly resumeStore = inject(ResumeStore);
   readonly resumes = toSignal(this.resumeStore.getAllResumes());
+  clipboard = inject(Clipboard);
+
+  copyResumeUrl(resumeId: string) {
+    const url = `${document.location.origin}/resume/edit/${resumeId}`;
+    this.clipboard.copy(url);
+  }
 }
